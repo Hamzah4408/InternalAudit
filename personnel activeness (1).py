6becab602f00e1494f534ef88ca4574addf6af2c
Hamzah4408 <119402192+Hamzah4408@users.noreply.github.com>
2025-10-10 14:36:19 +0800

    Update personnel activeness (1).py

--- personnel activeness (1).py
+++ personnel activeness (1).py
@@ -5,6 +5,8 @@ import pandas as pd
 import gradio as gr
 import os
 import tempfile
+from fastapi import FASTAPI
+from starlette.middleware.wsgi import WSGIMiddleware
 
 def find_email_column(df):
     for col in df.columns:
@@ -117,3 +119,6 @@ with gr.Blocks() as demo:
     dropdown.change(download_selected, inputs=[dropdown, file_state], outputs=download_file)
 
 demo.launch(share=True)
+
+app = FastAPI()
+app.mount("/", WSGIMiddleware(demo.app))
